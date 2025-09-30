54c152fb47d65b25fad26a5c55fdd986436272a8
MercyXp <munzenzimercy2024@gmail.com>
2025-09-30 17:33:21 +0200

    Project SetUp Complete

---
+++ frontend/src/components/Headers/LandingHeader.jsx
@@ -0,0 +1,10 @@
+// Header for non-logged-in pages
+import React from "react";
+import { Link } from "react-router-dom";
+
+function LandingHeader() {
+    return (
+        
+    );
+};
+export default LandingHeader;
