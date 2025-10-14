34e591e69ba00b29733687888103ded8d95d192b
MercyXp <munzenzimercy2024@gmail.com>
2025-10-14 20:16:53 +0200

    Fix: remove accounts.py url path from accounts/urls.py  and add to backend/urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from .views import RegisterView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -9,5 +9,4 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/auth/', include('accounts.urls')),
 ]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -37,4 +37,5 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/', include('accounts.urls')),
 ]
