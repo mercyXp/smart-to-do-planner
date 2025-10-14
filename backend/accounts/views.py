0547c6f254989c1109d317173644e55ddfdc893f
MercyXp <munzenzimercy2024@gmail.com>
2025-10-14 19:59:30 +0200

    Create authentication views — accounts/views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
+
