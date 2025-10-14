973861e0509b75bdce9dc8c6a944a302e1c5fcb5
MercyXp <munzenzimercy2024@gmail.com>
2025-10-14 19:34:01 +0200

    Create viewsets (ModelViewSet)

--- backend/goals/views.py
+++ backend/goals/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Goal
+from .serializers import GoalSerializer
 
-# Create your views here.
+class GoalViewSet(viewsets.ModelViewSet):
+    serializer_class = GoalSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Goal.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(goal__user=self.request.user)
