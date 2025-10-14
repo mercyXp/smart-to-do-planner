cd35a09420072c3448389a62f9543f0af112d5f1
MercyXp <munzenzimercy2024@gmail.com>
2025-10-14 20:06:07 +0200

    Add URLs

---
+++ backend/accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from .views import RegisterView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/', include('accounts.urls')),
+]
